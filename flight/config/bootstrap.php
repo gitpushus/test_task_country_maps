83f38392c3cee0cb2158c3a889e72616451c597d
gitpushus <119257424+gitpushus@users.noreply.github.com>
2025-04-15 17:49:41 +0300

    эндпоинты api

--- flight/config/bootstrap.php
+++ flight/config/bootstrap.php
@@ -1,7 +1,8 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-
 $dbConfig = require __DIR__ . '/database.php';
+
+use App\Infrastructure\Repositories\DatabaseAttractionRepository;
 use App\Infrastructure\Repositories\DatabaseCityRepository;
 Flight::register('db', PDO::class, [
     'mysql:host='.$dbConfig['host'].';dbname='.$dbConfig['name'],
@@ -10,5 +11,5 @@ Flight::register('db', PDO::class, [
     [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
 ]);
 Flight::register('cityRepository', DatabaseCityRepository::class, [Flight::db()]);
-
+Flight::register('attractionRepository', DatabaseAttractionRepository::class, [Flight::db()]);
 require __DIR__ . '/../routes/api.php';
--- flight/routes/api.php
+++ flight/routes/api.php
@@ -1,12 +1,17 @@
 <?php
+
+use App\Application\UseCases\CreateAttractionUseCase;
 use App\Application\UseCases\CreateCityUseCase;
+use App\Application\UseCases\DeleteAttractionUseCase;
 use App\Application\UseCases\DeleteCityUseCase;
+use App\Application\UseCases\GetAttractionsUseCase;
+use App\Application\UseCases\GetAttractionUseCase;
 use App\Application\UseCases\GetCitiesUseCase;
 use App\Application\UseCases\GetCityUseCase;
+use App\Application\UseCases\UpdateAttractionUseCase;
 use App\Application\UseCases\UpdateCityUseCase;
 use App\Presentation\Controllers\CityController;
 
-$getCityUseCase = new GetCityUseCase(Flight::cityRepository());
 Flight::register('getCitiesUseCase', GetCitiesUseCase::class, [Flight::cityRepository()]);
 Flight::register('getCityUseCase', GetCityUseCase::class, [Flight::cityRepository()]);
 Flight::register('createCityUseCase', CreateCityUseCase::class, [Flight::cityRepository()]);
@@ -32,3 +37,33 @@ Flight::route('GET /cities/@id:[0-9]+', [Flight::cityController(), 'show']);
 Flight::route('POST /cities', [Flight::cityController(), 'store']);
 Flight::route('PUT /cities/@id:[0-9]+', [Flight::cityController(), 'update']);
 Flight::route('DELETE /cities/@id:[0-9]+', [Flight::cityController(), 'destroy']);
+
+Flight::register('getAttractionsUseCase', GetAttractionsUseCase::class, [Flight::attractionRepository()]);
+Flight::register('getAttractionUseCase', GetAttractionUseCase::class, [Flight::attractionRepository()]);
+Flight::register('createAttractionUseCase', CreateAttractionUseCase::class, [
+    Flight::attractionRepository(),
+    Flight::getCityUseCase()
+]);
+Flight::register('updateAttractionUseCase', UpdateAttractionUseCase::class, [
+    Flight::cityRepository(),
+    Flight::getAttractionUseCase(),
+    Flight::getCityUseCase()
+]);
+Flight::register('deleteCityUseCase', DeleteAttractionUseCase::class, [
+    Flight::cityRepository(),
+    Flight::getAttractionUseCase()
+]);
+Flight::register('attractionController', CityController::class, [
+    Flight::getAttractionsUseCase(),
+    Flight::getAttractionUseCase(),
+    Flight::createAttractionseUseCase(),
+    Flight::updateAttractionseUseCase(),
+    Flight::deleteAttractionseUseCase(),
+    Flight::app()
+]);
+
+Flight::route('GET /attractions', [Flight::attractionController(), 'index']);
+Flight::route('GET /attractions/@id:[0-9]+', [Flight::attractionController(), 'show']);
+Flight::route('POST /attractions', [Flight::attractionController(), 'store']);
+Flight::route('PUT /attractions/@id:[0-9]+', [Flight::attractionController(), 'update']);
+Flight::route('DELETE /attractions/@id:[0-9]+', [Flight::attractionController(), 'destroy']);
