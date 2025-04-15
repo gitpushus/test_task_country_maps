34115d28bdfdff9e378d634a8de07fd77f549b55
gitpushus <119257424+gitpushus@users.noreply.github.com>
2025-04-15 16:05:42 +0300

    логика получения всех достопримечательностей

---
+++ flight/app/Application/UseCases/GetAttractionsUseCase.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Application\UseCases;
+
+use App\Domain\Entities\Attraction;
+use App\Domain\Repositories\AttractionRepositoryInterface;
+class GetAttractionsUseCase
+{
+    public function __construct(
+        private readonly AttractionRepositoryInterface $repository
+    ){}
+    public function execute(): array{
+        return $this->repository->getAll();
+    }
+}
