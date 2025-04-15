2c9d2809153bd89716c9423a2267ba5c210efe79
gitpushus <119257424+gitpushus@users.noreply.github.com>
2025-04-15 15:39:38 +0300

    регистрация подключения к бд и api

---
+++ flight/config/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$dbConfig = require __DIR__ . '/database.php';
+use App\Infrastructure\Repositories\DatabaseCityRepository;
+Flight::register('db', PDO::class, [
+    'mysql:host='.$dbConfig['host'].';dbname='.$dbConfig['name'],
+    $dbConfig['user'],
+    $dbConfig['pass'],
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+]);
+Flight::register('cityRepository', DatabaseCityRepository::class, [Flight::db()]);
+
+require __DIR__ . '/../routes/api.php';
